55d9e5ed43a874a24dfd628a088123606356f2b8
harsh-gymble <harsh.gymble@gmail.com>
2025-02-19 16:11:28 +0530

    fifth commit

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client"; // React 18 API
 import moment from "moment";
 import { X } from "lucide-react";
